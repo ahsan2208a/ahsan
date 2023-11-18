a4eb8f0ae75ecb4af383c1138146e872da1c2b66
Ahsan Hussain <Administrator@LAB6-PC4.labs.org>
2023-11-18 13:47:17 +0500

    Anything

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ServiceComponent } from './service/service.component';
+import { TeamComponent } from './team/team.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'service', component: ServiceComponent},
+  { path: 'portfolio', component: PortfolioComponent},
+  { path: 'team', component: TeamComponent},
+  { path: 'contact', component: ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ServiceComponent,
+    PortfolioComponent,
+    TeamComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
